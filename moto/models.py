86e247958d170f859f327c44bda3c9ea48af91f3
Robin Schroer <sulamiification@gmail.com>
2014-10-16 10:21:51 +0200

    refine bike model

--- moto/models.py
+++ moto/models.py
@@ -14,17 +14,30 @@ fairing_choices = (
     (4, 'full'),
 )
 
+ignition_choices = (
+    (1, 'carbureted'),
+    (2, 'fuel injected'),
+)
+
 class Bike(models.Model):
     cylinders = models.IntegerField()
     displacement = models.IntegerField()
     engineform = models.IntegerField(choices=form_choices)
     fairings = models.IntegerField(choices=fairing_choices)
     height = models.IntegerField()
+    ignition = models.IntegerField(choices=ignition_choices)
     make = models.CharField(max_length=20)
     model = models.CharField(max_length=30)
     power = models.IntegerField()
     price = models.IntegerField()
     release = models.IntegerField()
     seats = models.IntegerField()
+    strokes = models.IntegerField()
     weight = models.IntegerField()
+
+    # TODO
+    # Add variable features like electrical starters, etc...
+
+    def __str__(self):
+        return '{} {} {}'.format(self.release, self.make, self.model)
 
