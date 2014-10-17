c13c85ef804f2eca8df56f9fbf47454b6d936f41
Robin Schroer <sulamiification@gmail.com>
2014-10-17 12:25:56 +0200

    build huge view to construct a sql query

--- moto/views.py
+++ moto/views.py
@@ -7,35 +7,59 @@ def index(request):
     pass
 
 def table(request):
+
+    engineform = Q()
+    for i in request.GET.getlist('engineform'):
+        engineform |= Q(engineform__icontains=i)
+    fairings = Q()
+    for i in request.GET.getlist('fairings'):
+        fairings |= Q(fairings__icontains=i)
+    make = Q()
+    for i in request.GET.getlist('make'):
+        make |= Q(make__icontains=i)
+    seats = Q()
+    for i in request.GET.getlist('seats'):
+        seats |= Q(seats__icontains=i)
+
     q = Bike.objects.filter(
-        # TODO multiple choice queries
-
-        cylinders__gte=request.GET['min_cylinders'],
-        cylinders__lte=request.GET['max_cylinders'],
-        displacement__gte=request.GET['min_displacement'],
-        displacement__lte=request.GET['max_displacement'],
-
-        engineform = models.IntegerField(choices=form_choices, null=True)
-        fairings = models.IntegerField(choices=fairing_choices, null=True)
-
-        height__lte=request.GET['max_height'],
-        ignition=request.GET['ignition'],
-
-        make__icontains=request.GET['make'],
-
-        power__gte=request.GET['min_power'],
-        power__lte=request.GET['max_power'],
-        price__gte=request.GET['min_price'],
-        price__lte=request.GET['max_price'],
-
-        seats = models.IntegerField(null=True)
-
-        strokes=request.GET['strokes'],
-        weight__gte=request.GET['min_weight'],
-        weight__lte=request.GET['max_weight'],
-        year__gte=request.GET['min_year'],
-        year__lte=request.GET['max_year'],
+        engineform,
+        fairings,
+        make,
+        seats,
     )
+
+    if request.GET.get('min_cylinders'):
+        q.filter(cylinders__gte=request.GET.get('min_cylinders'))
+    if request.GET.get('max_cylinders'):
+        q.filter(cylinders__lte=request.GET.get('max_cylinders'))
+    if request.GET.get('min_displacement'):
+        q.filter(displacement__gte=request.GET.get('min_displacement'))
+    if request.GET.get('max_displacement'):
+        q.filter(displacement__lte=request.GET.get('max_displacement'))
+    if request.GET.get('max_height'):
+        q.filter(height__lte=request.GET.get('max_height'))
+    if request.GET.get('ignition'):
+        q = q.filter(ignition=request.GET.get('ignition'))
+    if request.GET.get('min_power'):
+        q.filter(power__gte=request.GET.get('min_power'))
+    if request.GET.get('max_power'):
+        q.filter(power__lte=request.GET.get('max_power'))
+    if request.GET.get('min_price'):
+        q.filter(price__gte=request.GET.get('min_price'))
+    if request.GET.get('max_price'):
+        q.filter(price__lte=request.GET.get('max_price'))
+    if request.GET.get('strokes'):
+        q.filter(strokes=request.GET.get('strokes'))
+    if request.GET.get('min_weight'):
+        q.filter(weight__gte=request.GET.get('min_weight'))
+    if request.GET.get('max_weight'):
+        q.filter(weight__lte=request.GET.get('max_weight'))
+    if request.GET.get('min_year'):
+        q.filter(year__gte=request.GET.get('min_year'))
+    if request.GET.get('max_year'):
+        q.filter(year__lte=request.GET.get('max_year'))
+
+    print q.query
 
 def item(request, id):
     pass
